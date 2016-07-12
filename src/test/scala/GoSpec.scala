fd90e26ab847541738433b86e179830f90380b55
James Turner <james.turner.phpninja@gmail.com>
2016-07-12 22:33:18 +0100

    Additional test for discounts

--- src/test/scala/GoSpec.scala
+++ src/test/scala/GoSpec.scala
@@ -7,21 +7,43 @@ class GoSpec extends FlatSpecLike with Matchers {
 
   val shopping =
     Table(
-      ("items", "total"),  // First tuple defines column names
-      (List("Apple","Apple","Orange","Apple"), 2.05),
-      (List("Apple","Apple","Apple"), 1.80),
+      ("items", "total"), // First tuple defines column names
+      (List("Apple", "Apple", "Orange", "Apple"), 2.05),
+      (List("Apple", "Apple", "Apple"), 1.80),
       (List(), 0.0) // empty cart
+    )
+
+  val discountShopping =
+    Table(
+      ("items", "total"),
+      (List("Apple", "Apple"), .6), // simple bogof
+      (List("Apple", "Apple", "Apple"), 1.2), // make sure we dont apply the discount again
+      (List("Orange", "Orange", "Orange"), .5), // simple 3 for 2
+      (List("Orange", "Orange"), .5), // no discount
+      (List("Orange", "Orange", "Orange", "Orange"), 0.75) // 1x 3-4-2 & 1 extra
     )
 
   it should "sum prices rights" in {
 
-    forAll(shopping) { (items: List[String], total: Double) =>
+    forAll(shopping) { (items, total) =>
 
       val cart = new ShoppingCart()
 
       val sum = cart.sumItems(items)
 
-      sum should be (total)
+      sum should be(total)
+    }
+
+  }
+
+  it should "apply discounts properly" in {
+
+    forAll(discountShopping) { (items, total) =>
+      val cart = new ShoppingCart()
+
+      val sum = cart.sumItems(items)
+
+      sum should be(total)
     }
 
   }
