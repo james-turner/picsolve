2bae214c840c7e735bafd3442eec35c073ee2e90
James Turner <james.turner.phpninja@gmail.com>
2016-07-12 22:56:43 +0100

    Simple implementation of bogof and 3-for-2

--- src/main/scala/ShoppingCart.scala
+++ src/main/scala/ShoppingCart.scala
@@ -7,9 +7,34 @@ class ShoppingCart {
   )
 
   def sumItems(items: List[String]): Double = {
-    items.map { s =>
-      prods(s).toDouble
-    }.sum/100
+    // can probably be rewritten as a for comprehension
+    Bogof("Apple").applyDiscounts(ThreeForTwo("Orange").applyDiscounts(items.map { s =>
+      (s, prods(s))
+    })).map(_._2).sum.toDouble/100
   }
 
+}
+
+trait Discount {
+  def applyDiscounts(items: List[(String,Int)]): List[(String,Int)]
+}
+
+case class ThreeForTwo(name: String) extends Discount{
+  override def applyDiscounts(items: List[(String, Int)]): List[(String, Int)] = {
+    val discountable = items.filter(_._1 == name)
+    val num = discountable.length / 3
+    items ::: (0 until num).map { i =>
+      (s"$name 3-for-2 discount", -discountable.head._2)
+    }.toList
+  }
+}
+
+case class Bogof(name: String) extends Discount {
+  override def applyDiscounts(items: List[(String, Int)]): List[(String, Int)] = {
+    val discountable = items.filter(_._1 == name)
+    val num = discountable.length / 2
+    items ::: (0 until num).map { i =>
+      (s"$name bogof discount", -discountable.head._2)
+    }.toList
+  }
 }
