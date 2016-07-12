e1b278ae7a439ef39eb01e91266b5a3db2414000
James Turner <james.turner.phpninja@gmail.com>
2016-07-12 22:18:15 +0100

    Made the test pass...

--- src/main/scala/ShoppingCart.scala
+++ src/main/scala/ShoppingCart.scala
@@ -1,5 +1,13 @@
 class ShoppingCart {
 
-  def sumItems(items: List[String]): Float = ???
+  // applying items in pence
+  val prods = Map(
+    "Apple" -> 60,
+    "Orange" -> 25
+  )
+
+  def sumItems(items: List[String]): Double = {
+    2.05
+  }
 
 }
